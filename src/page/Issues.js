b502de68ca440a6d09a37bf23060e2f90e18319f
kaen <kusdsuna@naver.com>
2021-11-14 18:06:55 +0900

    Fix: issue 리스트 생성 기능 구현

--- src/page/Issues.js
+++ src/page/Issues.js
@@ -59,11 +59,13 @@ const IssueBox = styled.li`
 
 const Isseus = () => {
 	const [issue, setIssue] = useState([]);
-	const [currentPage, setCurrentPage] = useState(1);
-	const [postsPerPage] = useState(10);
+	const [currentPage, setCurrentPage] = useState(1); // 현재 페이지
+	const [posts] = useState(10); // 페이지당 최대 게시글 수
 
 	// repoRegister 개수만큼 요청 후 해당 이슈만큼 map으로 뿌려줌
 	useEffect(() => {
+		// 처음 불러올때 초기화
+		let temp = [];
 		const username = localStorage.getItem("username");
 		const repoRegister = JSON.parse(localStorage.getItem("repoRegister"));
 
@@ -71,24 +73,34 @@ const Isseus = () => {
 			for (let i = 0; i < repoRegister.length; i++) {
 				let url = `https://api.github.com/repos/${username}/${repoRegister[i].name}/issues`;
 
-				axios
+				await axios
 					.get(url, {
 						headers: {
 							Accept: "application/vnd.github.nightshade-preview+json",
 						},
 					})
-					.then((res) => setIssue(res.data));
+					.then((res) =>
+						res.data.forEach((el) => {
+							el.repo = repoRegister[i].name;
+							temp.push(el);
+						})
+					);
 			}
+
+			// 더미데이터 생성용
+			for (let i = 0; i < 1000; i++) {
+				temp.push({});
+			}
+
+			setIssue(temp);
 		};
 
 		getRepo();
 	}, []);
 
-	const indexOfLastPost = currentPage * postsPerPage;
-	const indexOfFirstPost = indexOfLastPost - postsPerPage;
-	const currentPosts = issue.slice(indexOfFirstPost, indexOfLastPost);
-	console.log(currentPosts);
-
+	const lastPost = currentPage * posts;
+	const firstPost = lastPost - posts;
+	const currentPosts = issue.slice(firstPost, lastPost);
 	const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
 	return (
@@ -105,11 +117,7 @@ const Isseus = () => {
 					</IssueBox>
 				))}
 			</ul>
-			<Pagination
-				postsPerPage={postsPerPage}
-				totalPosts={issue.length}
-				paginate={paginate}
-			/>
+			<Pagination posts={posts} totalPosts={issue.length} paginate={paginate} />
 		</IssuesSection>
 	);
 };
--- src/page/Pagination.js
+++ src/page/Pagination.js
@@ -1,15 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+import { flexCenter, color } from "../components/utils/theme";
 
-const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+const PaginationSection = styled.ul`
+	${flexCenter}
+
+	background-color: gray;
+	width: 100%;
+	overflow: visible;
+
+	li {
+		margin: 1rem;
+		cursor: pointer;
+	}
+`;
+
+const Pagination = ({ posts, totalPosts, paginate }) => {
 	const pageNumber = [];
 
 	// Math.ceil: 올림
-	for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+	for (let i = 1; i <= Math.ceil(totalPosts / posts); i++) {
 		pageNumber.push(i);
 	}
 
 	return (
-		<ul className="pagination">
+		<PaginationSection>
 			{pageNumber.map((pageNum) => (
 				<li
 					key={pageNum}
@@ -19,7 +34,7 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
 					{pageNum}
 				</li>
 			))}
-		</ul>
+		</PaginationSection>
 	);
 };
 
